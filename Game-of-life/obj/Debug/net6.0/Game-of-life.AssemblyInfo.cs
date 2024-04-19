47479cc726cea10bfdf2a6c9b33e7e5ae575bce8
Troels Lund <trolund@gmail.com>
2024-04-19 20:53:22 +0200

    more

--- Game-of-life/obj/Debug/net6.0/Game-of-life.AssemblyInfo.cs
+++ Game-of-life/obj/Debug/net6.0/Game-of-life.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game-of-life")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b03c80785560c0456f42b264ad8e99569da92ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game-of-life")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game-of-life")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
